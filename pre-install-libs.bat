9efe741f6c8420386eb197c2a783cdc02c5e9337
Amelito Estrada <x209486@corp.ads>
2020-11-16 04:19:34 -0500

    pre-install-bat

---
+++ pre-install-libs.bat
@@ -0,0 +1,11 @@
+python -m pip install -Iv Django==2.1
+python -m pip install pdfminer
+python -m pip install sklearn
+python -m pip install spacy
+python -m pip install gender_detector
+python -m pip install nltk
+python -m pip install stop_words
+python -m pip install gensim
+python -m pip install python_docx
+python -m pip install language_check
+python -m spacy download en_core_web_sm
